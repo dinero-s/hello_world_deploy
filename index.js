60e29c5347e6e8897ce5546aff81ba4548d61a24
Linux User <linuxuser@vultr.guest>
2024-12-29 07:10:19 +0000

    commit 3.0

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 2000
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
