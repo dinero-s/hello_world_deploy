1ec52fd7d38c6c8090a66d77830e357a73943bd6
DINERO <dinero.int@mail.ru>
2024-12-29 11:08:46 +0300

    with test

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 2000
 
 app.get('/helloWorld', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).json({ message: 'Hello, world!' })
 })
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 } )
+
+module.exports = app
---
+++ index.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+const app = require('./index')
+
+describe('/hello endpoint', () => {
+  it('should return a 200 status and a message', async () => {
+    const response = await request(app).get('/helloWorld')
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({ message: 'Hello, world!' })
+  })
+})
