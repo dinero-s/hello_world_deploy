68246f5cf1945ecc16c3bef93f4068924345e845
DINERO <dinero.int@mail.ru>
2024-12-30 22:35:27 +0300

    added new default route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 2000
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from the root route!' });
+})
 
 app.get('/helloWorld', (req, res) => {
   res.status(200).json({ message: 'Hello, world!' })
